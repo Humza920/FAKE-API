3888ada47d646ba152af38ce4dc7170c45e0d255
Humza920 <humza.rehman.779@gmail.com>
2024-06-02 13:43:40 -0700

    DONE

---
+++ app.js
@@ -0,0 +1,29 @@
+const div = document.querySelector("div")
+
+
+axios("https://fakestoreapi.com/products")
+.then((res)=>{
+    console.log(res.data);
+    res.data.map((item)=>{
+        div.innerHTML+=`<div class ="items">
+        <h3 id ="you"> ${item.title}</h3> 
+        <img width = "150px" src =${item.image}>
+        <h3 id ="we">PRICE:</h3> <h5>${item.price}$<h5>
+        <h3 id ="we">Description:</h3><p class ="p1">${item.description}</p>
+        <div class ="d-flex justify-content-end"><button class ="btn btn-dark Cart" on-click ="">Add To Cart</button><b><hr class ="hr"></b></div>`
+    })
+})
+
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
+
+
+
+
+
+
+
