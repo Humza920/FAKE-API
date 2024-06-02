409735aed213112a0e03e19bc2559f6af39a5274
Humza920 <humza.rehman.779@gmail.com>
2024-06-02 13:59:32 -0700

    DONE

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const div = document.querySelector("div")
+const div = document.querySelector(".meta")
 
 
 axios("https://fakestoreapi.com/products")
@@ -7,7 +7,7 @@ axios("https://fakestoreapi.com/products")
     res.data.map((item)=>{
         div.innerHTML+=`<div class ="items">
         <h3 id ="you"> ${item.title}</h3> 
-        <img width = "150px" src =${item.image}>
+        <img width = "250px" src =${item.image}>
         <h3 id ="we">PRICE:</h3> <h5>${item.price}$<h5>
         <h3 id ="we">Description:</h3><p class ="p1">${item.description}</p>
         <div class ="d-flex justify-content-end"><button class ="btn btn-dark Cart" on-click ="">Add To Cart</button><b><hr class ="hr"></b></div>`
